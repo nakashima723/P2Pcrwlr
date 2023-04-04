11a3dc9e8aeae18244106779353bc78693134547
tenax66 <115792382+tenax66@users.noreply.github.com>
2023-04-04 22:40:57 +0900

    info.pyを削除

--- torrent/test_download_torrent.py
+++ torrent/test_download_torrent.py
@@ -2,7 +2,6 @@ import unittest
 import os
 import libtorrent as lt
 import urllib.request
-import info
 import client
 
 
@@ -22,10 +21,6 @@ class TestInfo(unittest.TestCase):
             url, os.path.join(self.TEST_DIR, self.FILE_NAME))
 
     # 現状、各メソッドを実行するだけで、assertionしていない。
-
-    def test_show_info(self):
-        info.show_info(lt.torrent_info(
-            os.path.join(self.TEST_DIR, self.FILE_NAME)))
 
     def test_client(self):
         cl = client.Client()
