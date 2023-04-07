86bd383ea17e34a18a86493230f303f19e245363
Nakashima723 <yokoshima723@gmail.com>
2023-04-07 12:33:37 +0900

    証拠採取タブでtorrentファイル詳細を表示
    「証拠採取を開始」のボタンも、アラートを表示したのち「採取状況」のタブに移動するようにしました。
    
    こちらでは実際の収集の動き（libtorrentを使用する部分）には触れない範囲で、続けてUIを作っていきます。
    ログファイルやフォルダの内容をもとに、ファイルを各タブに振り分ける動きや、ボタンを押したときの動作をやっていきます。

--- P2Pslyr.py
+++ P2Pslyr.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+import os
+import pathlib
+from torrentool.api import Torrent
+
+# 証拠ディレクトリへのパスを定義
+torrent_folder = os.path.join(pathlib.Path(__file__).parents[0], "evidence/torrent")
 
 def main():
     window = tk.Tk()
@@ -21,33 +27,97 @@ def main():
     notebook.pack(fill=tk.BOTH, expand=True)
 
     tab1 = ttk.Frame(notebook)
-    notebook.add(tab1, text='証拠採取')
-    
-    # リストボックスを含むキャンバス
-    suspect_canvas = tk.Canvas(tab1)
-    suspect_canvas.pack(fill=tk.BOTH, padx=(10, 10), pady=(0, 10), expand=True)
+    notebook.add(tab1, text='証拠採取')    
 
-    # キャンバス内にリストボックスを含むフレームを配置
-    suspect_listbox_frame = tk.Frame(suspect_canvas)
-    suspect_listbox_frame.pack(fill=tk.BOTH, expand=True)
+    # パネッドウィンドウの作成
+    paned_window = ttk.PanedWindow(tab1, orient=tk.VERTICAL)
+    paned_window.pack(fill=tk.BOTH, expand=True)
+
+    # リストボックスを含むフレーム
+    suspect_listbox_frame = tk.Frame(paned_window)
+    paned_window.add(suspect_listbox_frame)
 
     # リストボックス
-    keyword_listbox = tk.Listbox(suspect_listbox_frame, width=-1, height=4, font=small_font)
-    keyword_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    suspect_listbox = tk.Listbox(suspect_listbox_frame, width=-1, height=9, font=small_font)
+    suspect_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
     # スクロールバー
-    suspect_scrollbar = tk.Scrollbar(suspect_listbox_frame, orient=tk.VERTICAL, command=keyword_listbox.yview)
+    suspect_scrollbar = tk.Scrollbar(suspect_listbox_frame, orient=tk.VERTICAL, command=suspect_listbox.yview)
     suspect_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-    keyword_listbox.config(yscrollcommand=suspect_scrollbar.set)
+    suspect_listbox.config(yscrollcommand=suspect_scrollbar.set)
 
-    # サイズ変更用ウィジェット
-    def on_sizegrip_dragged(event):
-        keyword_listbox.config(height=keyword_listbox.size()+int(event.delta/10))
+    def names():
+        # torrentファイルに対応するフォルダ名を格納する配列
+        folder_names = []
+        # 現在の表示をクリア
+        suspect_listbox.delete(0, tk.END)
+        # torrent_folder 内のサブディレクトリを繰り返し処理
+        for subdir in os.listdir(torrent_folder):
+            subdir_path = os.path.join(torrent_folder, subdir)
+            
+            # サブディレクトリかどうかをチェック
+            if os.path.isdir(subdir_path):
+                torrent_file_path = os.path.join(subdir_path, "source.torrent")
+                
+                # source.torrent ファイルが存在するかチェック
+                if os.path.exists(torrent_file_path):
+                    # Torrent オブジェクトを作成し、ファイル名を抽出
+                    torrent = Torrent.from_file(torrent_file_path)
+                    for file_info in torrent.files:
+                        file_name = torrent.name
+                    split_string = subdir_path.replace("\\", "/").split("/")
+                    subdir_time = split_string[-1]
+                    date_parts = subdir_time.split('_')
+                    date_elements = date_parts[0].split('-')
+                    time_elements = date_parts[1].split('-')
+                    subdir_time = f"{date_elements[0]}年{date_elements[1]}月{date_elements[2]}日 {time_elements[0]}時{time_elements[1]}分{time_elements[2]}秒"                    
+                    list_name = file_name + " - " + subdir_time
+                    suspect_listbox.insert(tk.END, list_name)
+                    folder_names.append(torrent_file_path)
+    
+        def on_select(event):
+            index = suspect_listbox.curselection()[0]
+            torrent_file_path = folder_names[index]
 
-    suspect_sizegrip = ttk.Sizegrip(suspect_listbox_frame)
-    suspect_sizegrip.pack(side=tk.BOTTOM, anchor=tk.SE)
-    suspect_sizegrip.lift()
-    suspect_sizegrip.bind("<B1-Motion>", on_sizegrip_dragged)
+            # Torrent オブジェクトを作成
+            torrent = Torrent.from_file(torrent_file_path)
+
+            # トレントファイルに含まれる情報を表示
+            info_text.config(state=tk.NORMAL)
+            info_text.delete(1.0, tk.END)
+            def bytes_to_mb(size_in_bytes):
+                size_in_mb = size_in_bytes / (1024 * 1024)
+                return round(size_in_mb, 3)
+            # トレントファイルに含まれる情報を表示
+            info_text.insert(tk.END, f"ファイル名：{torrent.name}\n\n")
+            info_text.insert(tk.END, f"作成日時：{torrent.creation_date if torrent.creation_date else '不明'}\n")
+            info_text.insert(tk.END, f"作成者：{torrent.created_by if torrent.created_by else '不明'}\n")
+            info_text.insert(tk.END, f"コメント：{torrent.comment if torrent.comment else '不明'}\n") 
+            info_text.insert(tk.END, f"ファイルサイズ：{bytes_to_mb(torrent.total_size) if torrent.total_size else '不明'} MB\n")
+            info_text.insert(tk.END, f"ハッシュ：{torrent.info_hash if torrent.info_hash else '不明'}\n\n")            
+            info_text.insert(tk.END, f"トラッカー：{', '.join([url for sublist in torrent.announce_urls for url in sublist]) if torrent.announce_urls else '不明'}\n")
+            info_text.config(state=tk.DISABLED)
+        
+        suspect_listbox.bind("<<ListboxSelect>>", on_select)
+    names()
+
+    # テキストエリアを含むキャンバス
+    info_canvas = tk.Canvas(paned_window)
+    paned_window.add(info_canvas)
+
+    # キャンバス内にテキストエリアを含むフレームを配置
+    info_text_frame = tk.Frame(info_canvas)
+    info_text_frame.pack(fill=tk.BOTH, expand=True)
+
+    # テキストエリア
+    info_text = tk.Text(info_text_frame, wrap=tk.WORD, width=-1, height=7, font=small_font)
+    info_text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    info_text.insert(tk.END, "ここに選択したtorrentファイルの情報が表示されます。\n\n表示内容を見て、証拠採取を開始するかどうか決めてください。")
+
+    # スクロールバー
+    info_scrollbar = tk.Scrollbar(info_text_frame, orient=tk.VERTICAL, command=info_text.yview)
+    info_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    info_text.config(yscrollcommand=info_scrollbar.set, state=tk.DISABLED)
 
     # 編集用ボタン
     button_frame = tk.Frame(tab1)
@@ -59,7 +129,7 @@ def main():
     url_add_button = tk.Button(button_frame, text="URLから追加", font=small_font)
     url_add_button.pack(side=tk.LEFT, padx=(0, 10))
 
-    delete_button = tk.Button(button_frame, text="削除", font=small_font)
+    delete_button = tk.Button(button_frame, text="誤検出として記録", font=small_font)
     delete_button.pack(side=tk.LEFT, padx=(0, 10))
 
     def start_picking():
@@ -75,48 +145,67 @@ def main():
 
     start_button = tk.Button(button_frame, text="証拠採取を開始", font=font,command=start_picking)
     start_button.pack(side=tk.RIGHT, padx=(0, 10))
-
-    # リストボックスを含むキャンバス
-    info_canvas = tk.Canvas(tab1)
-    info_canvas.pack(fill=tk.BOTH, padx=(10, 10), pady=(0, 10), expand=True)
-
-    # キャンバス内にリストボックスを含むフレームを配置
-    info_text_frame = tk.Frame(info_canvas)
-    info_text_frame.pack(fill=tk.BOTH, expand=True)
-
-    # リストボックス
-    keyword_text = tk.Text(info_text_frame, wrap=tk.WORD, width=-1, height=7, font=small_font)
-    keyword_text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-    keyword_text.insert(tk.END, "ここにtorrentファイルの情報が表示されます。\nユーザーは表示内容を見て、証拠採取を開始するかどうか決めます。\n\n改行テスト\n改行テスト\n改行テスト\n改行テスト\n改行テスト\n改行テスト\n改行テスト\n改行テスト\n改行テスト\n改行テスト\n改行テスト\n改行テスト\n改行テスト\n改行テスト")
-
-
-    # スクロールバー
-    info_scrollbar = tk.Scrollbar(info_text_frame, orient=tk.VERTICAL, command=keyword_text.yview)
-    info_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-    keyword_text.config(yscrollcommand=info_scrollbar.set, state=tk.DISABLED)
-
-    # サイズ変更用ウィジェット
-    def on_sizegrip_dragged(event):
-        keyword_text.config(height=keyword_text.size()+int(event.delta/10))
-
-    info_sizegrip = ttk.Sizegrip(info_text_frame)
-    info_sizegrip.pack(side=tk.BOTTOM, anchor=tk.SE)
-    info_sizegrip.lift()
-    info_sizegrip.bind("<B1-Motion>", on_sizegrip_dragged)
+    
+    reflesh_button = tk.Button(button_frame, text="更新", font=small_font,command=names)
+    reflesh_button.pack(side=tk.RIGHT, padx=(0, 10))    
     
     tab2 = ttk.Frame(notebook)
     notebook.add(tab2, text='採取状況')
 
+    # パネッドウィンドウの作成
+    paned_window2 = ttk.PanedWindow(tab2, orient=tk.VERTICAL)
+    paned_window2.pack(fill=tk.BOTH, expand=True)
+
+    # リストボックスを含むフレーム
+    tracking_listbox_frame = tk.Frame(paned_window2)
+    paned_window2.add(tracking_listbox_frame)
+
+    # リストボックス
+    tracking_listbox = tk.Listbox(tracking_listbox_frame, width=-1, height=9, font=small_font)
+    tracking_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+    # スクロールバー
+    tracking_scrollbar = tk.Scrollbar(tracking_listbox_frame, orient=tk.VERTICAL, command=tracking_listbox.yview)
+    tracking_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    tracking_listbox.config(yscrollcommand=tracking_scrollbar.set)
+
+    # テキストエリアを含むキャンバス
+    process_canvas = tk.Canvas(paned_window2)
+    paned_window2.add(process_canvas)
+
+    # キャンバス内にテキストエリアを含むフレームを配置
+    process_text_frame = tk.Frame(process_canvas)
+    process_text_frame.pack(fill=tk.BOTH, expand=True)
+
+    # テキストエリア
+    process_text = tk.Text(process_text_frame, wrap=tk.WORD, width=-1, height=7, font=small_font)
+    process_text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    process_text.insert(tk.END, "ここに、選択したファイルの証拠採取の進行状況が表示されます。\n\nピース収集の進捗は完全にリアルタイムではなく、フォルダ内のログファイルから読み込まれます。\n最新の状況を知りたい場合は「更新」ボタンを押してください。")
+
+    # スクロールバー
+    process_scrollbar = tk.Scrollbar(info_text_frame, orient=tk.VERTICAL, command=process_text.yview)
+    process_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    process_text.config(yscrollcommand=info_scrollbar.set, state=tk.DISABLED)
+
+    # 編集用ボタン
+    button_frame2 = tk.Frame(tab2)
+    button_frame2.pack(fill=tk.X, pady=(0, 5))
+
+    restore_button = tk.Button(button_frame2, text="証拠採取を一時停止", font=font)
+    restore_button.pack(side=tk.RIGHT, padx=(0, 10))
+
     tab3 = ttk.Frame(notebook)
-    notebook.add(tab3, text='統計')
-    
+    notebook.add(tab3, text='誤検出')
+
     tab4 = ttk.Frame(notebook)
-    notebook.add(tab4, text='設定')    
+    notebook.add(tab4, text='統計')
+    
+    tab5 = ttk.Frame(notebook)
+    notebook.add(tab5, text='設定')    
 
     window.protocol("WM_DELETE_WINDOW", window.quit)
 
     window.mainloop()
     
-
 if __name__ == '__main__':
     main()
