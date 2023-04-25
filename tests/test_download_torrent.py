2e6b2f596f61af56a464f841673a6b0d90172c01
tenax66 <115792382+tenax66@users.noreply.github.com>
2023-04-25 21:46:23 +0900

    テストクラスで指定するパスを変更

--- tests/test_download_torrent.py
+++ tests/test_download_torrent.py
@@ -18,31 +18,31 @@ class TestClient(unittest.TestCase):
         url = 'https://webtorrent.io/torrents/big-buck-bunny.torrent'
 
         urllib.request.urlretrieve(
-            url, os.path.join(self.TEST_DIR, self.FOLDER_NAME, self.FILE_NAME))
+            url, os.path.join(self.TEST_DIR, self.FILE_NAME))
 
     # 現状、各メソッドを実行するだけで、assertionしていない。
 
     def test_download(self):
         cl = Client()
-        cl.download(os.path.join(self.TEST_DIR, self.FOLDER_NAME, self.FILE_NAME),
+        cl.download(os.path.join(self.TEST_DIR, self.FILE_NAME),
                     os.path.join(self.TEST_DIR, self.FOLDER_NAME))
 
     def test_fetch_peer_list(self):
         cl = Client()
         max_list_size = 10
-        peers = cl.fetch_peer_list(os.path.join(self.TEST_DIR, self.FOLDER_NAME, self.FILE_NAME), max_list_size)
+        peers = cl.fetch_peer_list(os.path.join(self.TEST_DIR, self.FILE_NAME), max_list_size)
         print(peers)
         self.assertTrue(len(peers) == max_list_size)
 
     def test_download_piece(self):
         cl = Client()
 
-        peers = cl.fetch_peer_list(os.path.join(self.TEST_DIR, self.FOLDER_NAME, self.FILE_NAME), max_list_size=5)
+        peers = cl.fetch_peer_list(os.path.join(self.TEST_DIR, self.FILE_NAME), max_list_size=5)
 
         for p in peers:
             print('download from {}'.format(p))
             cl.download_piece(
-                os.path.join(self.TEST_DIR, self.FOLDER_NAME, self.FILE_NAME),
+                os.path.join(self.TEST_DIR, self.FILE_NAME),
                 os.path.join(self.TEST_DIR, self.FOLDER_NAME),
                 0,
                 p
