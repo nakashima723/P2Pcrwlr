37848a039c25bca4ee788d11347a8fad849ea87f
Nakashima723 <yokoshima723@gmail.com>
2023-05-27 01:46:51 +0900

    予備のNTPサーバーから取得を試みる処理を追加
    その他、時刻関係の処理もこのファイルに統合しました。

--- utils/time.py
+++ utils/time.py
@@ -1,6 +1,26 @@
 import ntplib
 from datetime import datetime, timezone, timedelta
 
+def fetch_time():
+    # NTPサーバのリストを定義
+    ntp_servers = ['ntp.nict.jp', 'ntp.jst.mfeed.ad.jp', 'ntp.nifty.com', 'ntp1.jst.mfeed.ad.jp', 'ntp1.ocn.ne.jp']
+
+    # NTPサーバからUNIX時刻を取得するクライアントを初期化
+    ntp_client = ntplib.NTPClient()
+
+    unix_time = None
+    for ntp_server in ntp_servers:
+        try:
+            response = ntp_client.request(ntp_server)
+            unix_time = response.tx_time
+            break
+        except:
+            continue
+
+    if unix_time is None:
+        return 0
+    else:
+        return unix_time
 
 def fetch_jst() -> datetime:
     """
@@ -11,16 +31,20 @@ def fetch_jst() -> datetime:
     jst_time: datetime
         JSTを表すdatetime。
     """
-    # NTPサーバのアドレスを指定する
-    ntp_server = 'ntp.nict.jp'
-
-    # NTPサーバからUNIX時刻を取得する
-    ntp_client = ntplib.NTPClient()
-    response = ntp_client.request(ntp_server)
-    unix_time = response.tx_time
+    unix_time = fetch_time()
 
     # UNIX時刻をJSTに変換する
     jst = timezone(timedelta(hours=+9), 'JST')
     jst_time = datetime.fromtimestamp(unix_time, jst)
 
     return jst_time
+
+def utc_to_jst(datetime_utc):  
+    # UTCをJSTに変換  
+    utc = timezone.utc
+    jst = timezone(timedelta(hours=9))
+    
+    datetime_utc = datetime_utc.replace(tzinfo=utc)
+    datetime_jst = datetime_utc.astimezone(jst)
+    
+    return datetime_jst
