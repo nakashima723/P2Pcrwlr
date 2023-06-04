41b0f1bc12e6c0df3994fd445636a80f37e79855
Nakashima723 <yokoshima723@gmail.com>
2023-06-04 13:14:44 +0900

    ピースダウンロードの処理を並列化
    同時に10件までのピアを並行して処理するようにしました。

--- crawler/collector.py
+++ crawler/collector.py
@@ -1,7 +1,9 @@
 import os
 import pathlib
+import time
 import libtorrent as lt
 from torrent.client import Client
+from concurrent.futures import ThreadPoolExecutor
 
 EVIDENCE_FILE_PATH = os.path.join(pathlib.Path(__file__).parents[1], "evidence")
 SETTING_FOLDER = os.path.join(pathlib.Path(__file__).parents[1], "settings")
@@ -34,7 +36,7 @@ print(source_files)
 
 torrent_path = source_files[0]
 save_path = folder_list[0]
-max_list_size = 10
+max_list_size = 50
 piece_index = 0
 
 client = Client()
@@ -45,8 +47,15 @@ print("ピアの一覧を取得しています...")
 peers = client.fetch_peer_list(torrent_path, max_list_size)
 print("peers: " + str(peers))
 
-if(peers != []):
-    for i in range(len(peers)):
-        client.download_piece(torrent_path, save_path, piece_index, peers[i])
+def process_peer(peer):
+    client.download_piece(torrent_path, save_path, piece_index, peer)
+
+def process_peer(peer):
+    client.download_piece(torrent_path, save_path, piece_index, peer)
+
+if peers:
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for peer in peers:
+            executor.submit(process_peer, peer)
 else:
     print("対象となるピアがありませんでした。")
