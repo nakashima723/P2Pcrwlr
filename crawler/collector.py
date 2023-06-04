3d62caebf9cb023fa60970d571936250bbf9cdd4
Nakashima723 <yokoshima723@gmail.com>
2023-06-04 15:26:36 +0900

    ピース収集対象のフォルダすべてをループ処理

--- crawler/collector.py
+++ crawler/collector.py
@@ -1,27 +1,29 @@
 import os
-import pathlib
-import time
-import libtorrent as lt
+import sys
 from torrent.client import Client
 from concurrent.futures import ThreadPoolExecutor
 
-EVIDENCE_FILE_PATH = os.path.join(pathlib.Path(__file__).parents[1], "evidence")
-SETTING_FOLDER = os.path.join(pathlib.Path(__file__).parents[1], "settings")
+if getattr(sys, 'frozen', False):
+    # PyInstallerが使用する一時ディレクトリ
+    application_path = sys._MEIPASS
+else:
+    application_path = os.path.dirname(os.path.abspath(__file__))
+
+EVIDENCE_FILE_PATH = os.path.join(application_path, "evidence")
+SETTING_FOLDER = os.path.join(application_path, "settings")
 SETTING_FILE = os.path.join(SETTING_FOLDER, "setting.json")
 
-folder_list = []  # 「.processing」ファイルを含むフォルダパスのリスト
+folder_list = []  # 「.process」ファイルを含むフォルダパスのリスト
 
 # フォルダ内のすべてのサブフォルダをチェック
 for root, dirs, files in os.walk(EVIDENCE_FILE_PATH):
     # 各ファイルに対してチェック
     for file in files:
-        # ファイル名が「.processing」であるかどうかを確認
+        # ファイル名が「.process」であるかどうかを確認
         if file == ".process":
-            # 「.processing」ファイルを含むフォルダパスをリストに追加
+            # 「.process」ファイルを含むフォルダパスをリストに追加
             folder_list.append(root)
             break
-
-print(folder_list)
 
 source_files = []  # 「source.torrent」ファイルへのパスを格納するリスト
 
@@ -32,30 +34,21 @@ for folder in folder_list:
     # パスを「source_files」リストに追加
     source_files.append(source_file_path)
 
-print(source_files)
-
-torrent_path = source_files[0]
-save_path = folder_list[0]
 max_list_size = 50
-piece_index = 0
 
 client = Client()
 
-#client.download(torrent_path, save_path)
+for i in range(len(source_files)):    
+    client.download(source_files[i], folder_list[i])
+    print("ピアの一覧を取得しています...")
+    peers = client.fetch_peer_list(source_files[i], max_list_size)
+    print("peers: " + str(peers))
 
-print("ピアの一覧を取得しています...")
-peers = client.fetch_peer_list(torrent_path, max_list_size)
-print("peers: " + str(peers))
+    def process_peer(peer):
+        client.download_piece(source_files[i], folder_list[i], peer)
 
-def process_peer(peer):
-    client.download_piece(torrent_path, save_path, piece_index, peer)
-
-def process_peer(peer):
-    client.download_piece(torrent_path, save_path, piece_index, peer)
-
-if peers:
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        for peer in peers:
-            executor.submit(process_peer, peer)
-else:
-    print("対象となるピアがありませんでした。")
+    if peers:
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            executor.map(process_peer, peers)
+    else:
+        print("対象となるピアがありませんでした。")
