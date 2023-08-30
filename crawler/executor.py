9c45e7103f45835b0ef507353ed5e7639fb03f85
Nakashima723 <yokoshima723@gmail.com>
2023-08-30 21:42:17 +0900

    コメントを日本語化

--- crawler/executor.py
+++ crawler/executor.py
@@ -5,26 +5,25 @@ from tkinter import filedialog
 
 def select_folder(title=None):
     root = tk.Tk()
-    root.withdraw()  # Hide the main window
-    folder_path = filedialog.askdirectory(title=title)  # Show folder selection dialog
+    root.withdraw()  # メインウィンドウを非表示にする
+    folder_path = filedialog.askdirectory(title=title)  # フォルダ選択ダイアログを表示
     return folder_path
 
-# Select the source folder (the folder containing the target DL file)
+# 検証したいDL対象ファイル本体と、DL元になったtorrentファイルが存在するフォルダの選択
 source_folder = select_folder("検証したいDL対象ファイル本体と、DL元になったtorrentファイルが存在するフォルダを選択")
-print("対象フォルダの一覧を作成しています……")
 
-# List all sub-folders under the source folder which contain .bin files
+# source_folderの下の.binファイルを含むすべてのサブフォルダのリスト
 sub_folders = [os.path.join(source_folder, d) for d in os.listdir(source_folder) if os.path.isdir(os.path.join(source_folder, d))]
 piece_folders_list = [f for f in sub_folders if any(fn.endswith('.bin') for fn in os.listdir(f))]
 
-# Initialize results dictionary
+# 結果を保存するための辞書の初期化
 results = {}
 
 print("バイナリマッチを試行中……")
 
-# Iterate through each piece folder and .bin file
+# 各ピースフォルダと.binファイルを反復処理
 for piece_folder in piece_folders_list:
-    # Create an instance of BinaryMatcher for each piece folder
+    # 各ピースフォルダに対してBinaryMatcherのインスタンスを作成
     matcher = BinaryMatcher(source_folder, piece_folder)
     
     bin_files = [f for f in os.listdir(piece_folder) if f.endswith('.bin') and os.path.isfile(os.path.join(piece_folder, f))]
@@ -32,11 +31,11 @@ for piece_folder in piece_folders_list:
         result = matcher.binary_match(bin_file)
         results[bin_file] = result
 
-# Check the results
-if not any(value == False for value in results.values()):
+# 結果の確認
+if not any(isinstance(value, bool) and value is False for value in results.values()):
     print("すべてのピースが元ファイルの内容と一致しました。")
 else:
-    mismatched_pieces = [key for key, value in results.items() if value == False]
+    mismatched_pieces = [key for key, value in results.items() if isinstance(value, bool) and value is False]
     print("次のピースが元ファイルの内容と一致しませんでした：")
     for piece in mismatched_pieces:
         print(piece)
