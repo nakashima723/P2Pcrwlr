b3b01e3b71162c8ba0a4a9a0499dc4fea0c6d310
Nakashima723 <yokoshima723@gmail.com>
2023-09-13 11:54:08 +0900

    flake8エラーを修正

--- utils/binary_matcher.py
+++ utils/binary_matcher.py
@@ -13,7 +13,7 @@ class BinaryMatcher:
         # トレントファイルの読み込み
         torrent_file = "source.torrent"
         file_path = os.path.join(self.source_folder, torrent_file)
-        
+
         # ファイル存在チェック
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"{torrent_file} が見つかりません。")
@@ -25,7 +25,7 @@ class BinaryMatcher:
         # ダウンロード対象のファイルまたはフォルダ名の取得
         download_file_name = self.torrent_data[b"info"][b"name"].decode("utf-8", errors='replace')
         download_file = os.path.join(self.source_folder, download_file_name)
-        
+
         # ファイル存在チェック
         if not os.path.exists(download_file):
             raise FileNotFoundError(f"{download_file_name} が見つかりません。")
@@ -94,7 +94,7 @@ class PeerBinaryMatcher:
                        and "_" in item]
 
         matcher = BinaryMatcher(self.folder, None)  # 先にインスタンスを作成
-        
+
         print("バイナリマッチを実行中...（計" + str(len(all_folders)) + "件）")
 
         for folder in all_folders:
@@ -103,7 +103,7 @@ class PeerBinaryMatcher:
 
             current_peer_folder = os.path.join(self.folder, folder)
             log_files = [f for f in os.listdir(current_peer_folder) if f.endswith('.log')]
-            
+
             if not log_files:
                 # ログファイルが存在しない場合、新しいログファイルを作成
                 error_log_path = os.path.join(current_peer_folder, f"エラー_ログファイルが存在しません_{folder}.log")
@@ -127,7 +127,7 @@ class PeerBinaryMatcher:
                 os.rename(os.path.join(current_peer_folder, bin_file), new_name)
 
         print("バイナリマッチによる検証が完了しました。")
-        
+
         if not mismatched_files:
             print("\nすべてのピースが元ファイルの内容と一致しました。")
 
