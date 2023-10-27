a3ec7c07520bc190bf55408997c78e1184b1d988
Nakashima723 <yokoshima723@gmail.com>
2023-10-27 20:48:51 +0900

    バイナリ検査モジュールを修正
    通信エラーによりNullバイトで埋まっているピースが検出できない問題を修正

--- chore/binary_match_tester.py
+++ chore/binary_match_tester.py
@@ -24,34 +24,60 @@ piece_folders_list = [
     f for f in sub_folders if any(fn.endswith(".bin") for fn in os.listdir(f))
 ]
 
-# 結果を保存するための辞書の初期化
-results = {}
-
-print("バイナリマッチを試行中……")
-
-# 各ピースフォルダと.binファイルを反復処理
-for piece_folder in piece_folders_list:
-    # 各ピースフォルダに対してBinaryMatcherのインスタンスを作成
-    matcher = BinaryMatcher(source_folder, piece_folder)
-
-    bin_files = [
-        f
-        for f in os.listdir(piece_folder)
-        if f.endswith(".bin") and os.path.isfile(os.path.join(piece_folder, f))
-    ]
-    for bin_file in bin_files:
-        result = matcher.binary_match(bin_file)
-        results[bin_file] = result
-
-# 結果の確認
-if not any(isinstance(value, bool) and value is False for value in results.values()):
-    print("すべてのピースが元ファイルの内容と一致しました。")
+if not piece_folders_list:
+    print("マッチ対象となるフォルダ・ピースファイルが見つかりませんでした。\n「ピースファイルをサブフォルダ内に含む」フォルダを選択してください。")
 else:
+    # 結果を保存するための辞書の初期化
+    results = {}
+
+    print("バイナリマッチを試行中……")
+
+    # 各ピースフォルダと.binファイルを反復処理
+    for piece_folder in piece_folders_list:
+        # 各ピースフォルダに対してBinaryMatcherのインスタンスを作成
+        matcher = BinaryMatcher(source_folder, piece_folder)
+
+        bin_files = [
+            f
+            for f in os.listdir(piece_folder)
+            if f.endswith(".bin") and os.path.isfile(os.path.join(piece_folder, f))
+        ]
+        print(piece_folder)
+        for bin_file in bin_files:
+            result = matcher.binary_match(bin_file)
+
+            # resultが0で、かつbin_fileの中身がすべて空白である場合
+            if result == 0:
+                with open(os.path.join(piece_folder, bin_file), "rb") as f:
+                    file_content = f.read()
+                if all(
+                    b == 0x20 or b == 0x00 for b in file_content
+                ):  # すべてのバイトが空白（0x20）またはNullバイト（0x00）であるか
+                    result = None
+
+            results[bin_file] = result
+
+    # 結果の確認
+    # valueがFalseであるresults.items()の中身をmismatched_piecesとしてリストに収録
     mismatched_pieces = [
         key
         for key, value in results.items()
         if isinstance(value, bool) and value is False
     ]
-    print("次のピースが元ファイルの内容と一致しませんでした：")
-    for piece in mismatched_pieces:
-        print(piece)
+
+    # valueがNoneであるresults.items()の中身をblank_piecesとしてリストに収録
+    blank_pieces = [key for key, value in results.items() if value is None]
+
+    # mismatched_piecesとblank_piecesが存在しない場合、すべてのピースが一致したと表示
+    if not mismatched_pieces and not blank_pieces:
+        print("すべてのピースが元ファイルの内容と一致しました。")
+    else:
+        if mismatched_pieces:
+            print("次のピースは元ファイルの内容と一致しませんでした：")
+            for piece in mismatched_pieces:
+                print(piece)
+
+        if blank_pieces:
+            print("次のピースは空白で占められたファイルでした。piece_indexが0ではないなら、通信エラーの可能性があります。")
+            for piece in blank_pieces:
+                print(piece)
